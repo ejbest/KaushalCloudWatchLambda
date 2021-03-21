a254e667baad30d44f21106500e3f95097505893
A4business <30354660+a4business@users.noreply.github.com>
2021-03-21 17:06:51 +0200

    Update create.sh
    fix errors

--- create.sh
+++ create.sh
@@ -2,7 +2,7 @@
 
 ## Remove old files
 script=ej_lambda.py
-[ ! -f ${script}  ] && echo "Failed to locate file with function:  ${script}!" && return -1
+[ ! -f ${script}  ] && echo "Failed to locate file with function:  ${script}!" && exit  -1
 [ -f ${script}.zip ] && rm -rf ${script}.zip
 
 echo "******************************************"
@@ -14,13 +14,13 @@ echo -n " ## Create role: "
 policy='{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'
 myarn=`aws iam create-role --role-name ej_lambda --assume-role-policy-document ${policy} 2>/dev/null| perl -lne 'print if s/\|\|\s+(Arn)\s+\|(.*)\|\|/\2/' |tr -d ' '`
     
-[ ! -z $myarn ] && ( myid=`echo $myarn|cut -d':' -f5`; echo " ...OK: [myid=${myid}; myarn=${myarn}]") || (echo '  ... FAILED Creating role!';return -1)
+[ ! -z $myarn ] && ( myid=`echo $myarn|cut -d':' -f5`; echo " ...OK: [myid=${myid}; myarn=${myarn}]") || (echo '  ... FAILED Creating role!';exit -1)
 
 echo -n " ## Attach role, and wait 10sec: "
 aws iam attach-role-policy \
     --role-name ej_lambda \
     --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole && sleep 10
-echo " ...OK"
+echo "==============="
 
 echo -n " ## Create function from ${script},invoke it: "
 zip ${script}.zip ${script}
@@ -31,13 +31,13 @@ aws lambda create-function \
     --handler ej_lambda.ej_lambda \
     --role $myarn 
 aws lambda invoke --function-name ej_lambda response.json
-echo " ...OK"
+echo "==============="
 
 echo -n "  ## Create cloudwatch rule: "
 aws events put-rule \
     --name event5 \
     --schedule-expression 'cron(04 01 * * ? *)'
-echo " ...OK"
+echo "==============="
 
 echo -n "  ## Add permission "
 aws lambda add-permission \
@@ -46,7 +46,7 @@ aws lambda add-permission \
     --action 'lambda:InvokeFunction' \
     --principal events.amazonaws.com \
     --source-arn arn:aws:events:us-east-1:${myid}:rule/event5
-echo " ...OK"    
+echo "==============="
 
 
 
