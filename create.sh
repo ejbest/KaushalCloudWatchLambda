7e11f74b244f536c758f42faa2b1c77ff2fbc9e1
A4business <30354660+a4business@users.noreply.github.com>
2021-03-21 16:56:00 +0200

    Update create.sh
    Script modification for better look

--- create.sh
+++ create.sh
@@ -1,59 +1,60 @@
 #!/bin/bash
-set +x
-rm -Rf ej_lambda.zip
-rm -Rf foo
+
+## Remove old files
+script=ej_lambda.py
+[ ! -f ${script}  ] && echo "Failed to locate file with function:  ${script}!" && return -1
+[ -f ${script}.zip ] && rm -rf ${script}.zip
+
 echo "******************************************"
 echo "*** iam role ej_lambda  ******************"
 echo "******************************************"
-echo "aws iam create-role --role-name ej_lambda --assume-role-policy-document '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}' "
-aws iam create-role --role-name ej_lambda --assume-role-policy-document '{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}' > foo
-set +x
-mystr="`grep "arn:aws" foo`"
-mymid=`echo $mystr | cut -d ":" -f 2-25`
-myarn=`echo $mymid | tr -d '",'`
-myarn="${myarn}/service-role/ej_lambda"
-echo "mystr.....$mystr"
-echo "mymid.....$mymid"
-myarn=`cat foo|perl -lne 'print if s/\|\|\s+(Arn)\s+\|(.*)\|\|/\2/' |tr -d ' '` && echo "mystr....$mystr..."
-#arn="arn:aws:iam::362863965643:role/ej_lambda"
-echo "******************************************"
-echo "myarn.....$myarn....."
-#echo "arn.......$arn....."
-echo "******************************************"
-echo "*** iam policy ***************************"
-echo "******************************************"
-echo "aws iam attach-role-policy --role-name ej_lambda --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-aws iam attach-role-policy --role-name ej_lambda --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
-sleep 10
-echo "******************************************"
-echo "******************************************"
-echo "******************************************"
-zip ej_lambda.zip ej_lambda.py
-echo "******************************************"
-echo "******************************************"
-echo "******************************************"
-echo "aws lambda create-function --function-name ej_lambda --runtime python3.8 --zip-file fileb://ej_lambda.zip --handler ej_lambda.handler --role $myarn" 
-aws lambda create-function --function-name ej_lambda --runtime python3.8 --zip-file fileb://ej_lambda.zip --handler ej_lambda.ej_lambda --role $myarn
+echo
+
+echo -n " ## Create role: "
+policy='{"Version": "2012-10-17","Statement": [{ "Effect": "Allow", "Principal": {"Service": "lambda.amazonaws.com"}, "Action": "sts:AssumeRole"}]}'
+myarn=`aws iam create-role \
+    --role-name ej_lambda \
+    --assume-role-policy-document ${policy}` 2>/dev/null| perl -lne 'print if s/\|\|\s+(Arn)\s+\|(.*)\|\|/\2/' |tr -d ' '`
+    
+[ ! -z $myarn ] && ( myid=`echo $myarn|cut -d':' -f5`; echo " ...OK: [myid=${myid}; myarn=${myarn}]") || (echo '  ... FAILED Creating role!';return -1)
+
+echo -n " ## Attach role, and wait 10sec: "
+aws iam attach-role-policy \
+    --role-name ej_lambda \
+    --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole && sleep 10
+echo " ...OK"
+
+echo -n " ## Create function from ${script},invoke it: "
+zip ${script}.zip ${script}
+aws lambda create-function \
+    --function-name ej_lambda \
+    --runtime python3.8 \
+    --zip-file fileb://${script}.zip \
+    --handler ej_lambda.ej_lambda \
+    --role $myarn 
 aws lambda invoke --function-name ej_lambda response.json
-echo "******************************************"
-echo "*** event in cloudwatch ******************"
-echo "******************************************"
-# #create cloudwatch rule
+echo " ...OK"
+
+echo -n "  ## Create cloudwatch rule: "
 aws events put-rule \
     --name event5 \
     --schedule-expression 'cron(04 01 * * ? *)'
-echo "******************************************"
-echo "*** permission in lambda *****************"
-echo "******************************************"
+echo " ...OK"
+
+echo -n "  ## Add permission "
 aws lambda add-permission \
     --function-name ej_lambda \
     --statement-id event5 \
     --action 'lambda:InvokeFunction' \
     --principal events.amazonaws.com \
-    --source-arn arn:aws:events:us-east-1:362863965643:rule/event5
+    --source-arn arn:aws:events:us-east-1:${myid}:rule/event5
+echo " ...OK"    
+
+
+
+
 
 # aws events put-targets --rule event5 --targets file://target.json
-
 # ===================================================================
 # First run
 # #aws iam create-role --role-name ej_lambda --assume-role-policy-document file://trust-policy.json
